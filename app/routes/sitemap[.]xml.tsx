9d7a845b0189cf9d9cdd8c3b58be526806e16ef4
Davide Giovanni Steccanella <33760151+davidegiovanni@users.noreply.github.com>
2022-10-04 22:16:54 +0200

    fix: sitemap

--- app/routes/sitemap[.]xml.tsx
+++ app/routes/sitemap[.]xml.tsx
@@ -49,32 +49,34 @@ export const loader: LoaderFunction = async ({ request }) => {
   const content = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     ${locales.map((l: any) => (`<url>
-        <loc>https://${websiteName}/${l}</loc>
-        <lastmod>2022-01-01T00:00:00+01:00</lastmod>${getAlternateLocales(l).map(al => (`
-        <xhtml:link rel="alternate" hreflang="${al}" href="https://${websiteName}/${al}"/>`)).toString().split(',').join('')}
-        <priority>1.0</priority>
-      </url>`)).toString().split(',').join('')}
-      <url>
-        <loc>https://${websiteName}/it-IT/works</loc>
-        <lastmod>2022-01-01T00:00:00+01:00</lastmod>
-        <priority>1.0</priority>
-      </url>
-      <url>
-        <loc>https://${websiteName}/it-IT/contacts</loc>
-        <lastmod>2022-01-01T00:00:00+01:00</lastmod>
-        <priority>1.0</priority>
-      </url>
-      <url>
-        <loc>https://${websiteName}/it-IT/about</loc>
-        <lastmod>2022-01-01T00:00:00+01:00</lastmod>
-        <priority>1.0</priority>
-      </url>
-    ${itFeeds.map((feed) => feed.items.map((item) => `<url>
-        <loc>https://${websiteName}/it-IT/works/${feed.title.toLowerCase().split(' ').join('-')}/${getSlug(item.id)}</loc>
-        <lastmod>${item.date_published}</lastmod>
-        <priority>1.0</priority>
-      </url>`)).toString().split(',').join('')}
-    ${itFeeds.map((feed) =>(`<url>
+      <loc>https://${websiteName}/${l}</loc>
+      <lastmod>2022-01-01T00:00:00+01:00</lastmod>${getAlternateLocales(l).map(al => (`
+      <xhtml:link rel="alternate" hreflang="${al}" href="https://${websiteName}/${al}"/>`)).toString().split(',').join('')}
+      <priority>1.0</priority>
+    </url>`)).toString().split(',').join('')}
+    <url>
+      <loc>https://${websiteName}/it-IT/works</loc>
+      <lastmod>2022-01-01T00:00:00+01:00</lastmod>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://${websiteName}/it-IT/contacts</loc>
+      <lastmod>2022-01-01T00:00:00+01:00</lastmod>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://${websiteName}/it-IT/about</loc>
+      <lastmod>2022-01-01T00:00:00+01:00</lastmod>
+      <priority>1.0</priority>
+    </url>
+  ${itFeeds.map((feed) => feed.items.map((item) => 
+    `<url>
+      <loc>https://${websiteName}/it-IT/works/${feed.title.toLowerCase().split(' ').join('-')}/${getSlug(item.id)}</loc>
+      <lastmod>${item.date_published}</lastmod>
+      <priority>1.0</priority>
+    </url>`)).toString().split(',').join('')}
+  ${itFeeds.map((feed) =>(
+    `<url>
       <loc>https://${websiteName}/it-IT/works/${feed.title.toLowerCase().split(' ').join('-')}</loc>
       <lastmod>2022-01-01T00:00:00+01:00</lastmod>
       <priority>1.0</priority>
