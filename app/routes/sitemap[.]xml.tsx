f6691717cb7c46557af75f0cc857453bd2d05e88
Davide Giovanni Steccanella <33760151+davidegiovanni@users.noreply.github.com>
2022-10-04 22:15:15 +0200

    fix: sitemap

--- app/routes/sitemap[.]xml.tsx
+++ app/routes/sitemap[.]xml.tsx
@@ -46,40 +46,40 @@ export const loader: LoaderFunction = async ({ request }) => {
     return parsed.content
   }
 
-  const content = `<?xml version="1.0 " encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-  ${locales.map((l: any) => (`<url>
-      <loc>https://${websiteName}/${l}</loc>
-      <lastmod>2022-01-01T00:00:00+01:00</lastmod>${getAlternateLocales(l).map(al => (`
-      <xhtml:link rel="alternate" hreflang="${al}" href="https://${websiteName}/${al}"/>`)).toString().split(',').join('')}
+  const content = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    ${locales.map((l: any) => (`<url>
+        <loc>https://${websiteName}/${l}</loc>
+        <lastmod>2022-01-01T00:00:00+01:00</lastmod>${getAlternateLocales(l).map(al => (`
+        <xhtml:link rel="alternate" hreflang="${al}" href="https://${websiteName}/${al}"/>`)).toString().split(',').join('')}
+        <priority>1.0</priority>
+      </url>`)).toString().split(',').join('')}
+      <url>
+        <loc>https://${websiteName}/it-IT/works</loc>
+        <lastmod>2022-01-01T00:00:00+01:00</lastmod>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>https://${websiteName}/it-IT/contacts</loc>
+        <lastmod>2022-01-01T00:00:00+01:00</lastmod>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>https://${websiteName}/it-IT/about</loc>
+        <lastmod>2022-01-01T00:00:00+01:00</lastmod>
+        <priority>1.0</priority>
+      </url>
+    ${itFeeds.map((feed) => feed.items.map((item) => `<url>
+        <loc>https://${websiteName}/it-IT/works/${feed.title.toLowerCase().split(' ').join('-')}/${getSlug(item.id)}</loc>
+        <lastmod>${item.date_published}</lastmod>
+        <priority>1.0</priority>
+      </url>`)).toString().split(',').join('')}
+    ${itFeeds.map((feed) =>(`<url>
+      <loc>https://${websiteName}/it-IT/works/${feed.title.toLowerCase().split(' ').join('-')}</loc>
+      <lastmod>2022-01-01T00:00:00+01:00</lastmod>
       <priority>1.0</priority>
     </url>`)).toString().split(',').join('')}
-    <url>
-      <loc>https://${websiteName}/it-IT/works</loc>
-      <lastmod>2022-01-01T00:00:00+01:00</lastmod>
-      <priority>1.0</priority>
-    </url>
-    <url>
-      <loc>https://${websiteName}/it-IT/contacts</loc>
-      <lastmod>2022-01-01T00:00:00+01:00</lastmod>
-      <priority>1.0</priority>
-    </url>
-    <url>
-      <loc>https://${websiteName}/it-IT/about</loc>
-      <lastmod>2022-01-01T00:00:00+01:00</lastmod>
-      <priority>1.0</priority>
-    </url>
-  ${itFeeds.map((feed) => feed.items.map((item) => `<url>
-      <loc>https://${websiteName}/it-IT/works/${feed.title.toLowerCase().split(' ').join('-')}/${getSlug(item.id)}</loc>
-      <lastmod>${item.date_published}</lastmod>
-      <priority>1.0</priority>
-    </url>`)).toString().split(',').join('')}
-  ${itFeeds.map((feed) =>(`<url>
-    <loc>https://${websiteName}/it-IT/works/${feed.title.toLowerCase().split(' ').join('-')}</loc>
-    <lastmod>2022-01-01T00:00:00+01:00</lastmod>
-    <priority>1.0</priority>
-  </url>`)).toString().split(',').join('')}
-</urlset>`.trim()
+  </urlset>`.trim()
 
   // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
   return new Response(content, {
