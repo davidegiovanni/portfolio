3142533049596f5d79dc253e541822f65101ae76
Davide <33760151+davidegiovanni@users.noreply.github.com>
2024-04-03 13:10:24 +0200

    fix: themes public key

--- app/services/api/headers-decoder/index.ts
+++ app/services/api/headers-decoder/index.ts
@@ -1,6 +1,6 @@
 export const headersDecoder = (request: Request) => {
   return {
-    websiteName: request.headers.get("Revas-Authority") || process.env.REVAS_OS_WEB_UI_WEBSITE_NAME || "illustrations.davidegiovanni.com",
+    websiteName: request.headers.get("Revas-Authority") || process.env.REVAS_OS_WEB_UI_WEBSITE_NAME || "01htht6y7643ty0ztaweztdgr0",
     publicKey: request.headers.get("Revas-Public-Key") || process.env.REVAS_OS_WEB_UI_PUBLIC_KEY || "01exy3y9j9pdvyzhchkpj9vc5w",
   };
 };
